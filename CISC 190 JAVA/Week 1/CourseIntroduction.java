71d8d346c7fd26035f3aa5597e622538998722e4
Mobinsqueue <150966273+Mobinsqueue@users.noreply.github.com>
2026-08-24 15:11:49 -0700

    Add files via upload
    W1-1/4 Java environment

---
+++ CISC 190 JAVA/Week 1/CourseIntroduction.java
@@ -0,0 +1,22 @@
+// Class Declaration
+//opening bracket after class declaration
+public class CourseIntroduction {
+
+    //Main Method
+    public static void main(String[] args) {
+        //output for name
+        //use "" for strings also known as stringliteral
+        System.out.println("My name is Mobin Sanaei");
+
+        //output for course
+        System.out.println("Course : Java Progaramming");
+
+        //output for goal
+        System.out.println("Goal : learn the basics of java");
+
+        //just for testing
+        System.out.println();
+    }
+
+}
+//closing brackets one for method and one for class
--- CISC 190 JAVA/Week 1/HelloJava.java
+++ CISC 190 JAVA/Week 1/HelloJava.java
@@ -1,5 +1,8 @@
 public class HelloJava {
-  public static void main(string[] args) {
-                          system.out.println("hello, java!");
-  }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+        System.out.println("I am learning Java Programming.");
+        System.out.println("Practice makes programming easier.");
+    }
 }
