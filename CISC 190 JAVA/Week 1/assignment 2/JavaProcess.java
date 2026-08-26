faccdeab4ba0a6af6b65fbb07840657643957620
Mobinsqueue <150966273+Mobinsqueue@users.noreply.github.com>
2026-08-25 20:56:08 -0700

    Add files via upload

---
+++ CISC 190 JAVA/Week 1/assignment 2/JavaProcess.java
@@ -0,0 +1,10 @@
+public class JavaProcess {
+
+    // 1 : source file by programmer is stored in .java file
+    // 2 : converts source code into bytecode that computer can run
+    // 3 : compiled converted bytecode is stored
+    // 4 : runs byte code in .class file
+    public static void main(String[] args) {
+        System.out.println("I understand the Java Compilation Process.");
+    }
+}
