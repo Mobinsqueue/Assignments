2df7912413320271bcf3bd9d8afcadc2dbaaa38b
Mobinsqueue <150966273+Mobinsqueue@users.noreply.github.com>
2026-08-25 20:50:27 -0700

    Add files via upload

---
+++ CISC 190 JAVA/Week 1/CompilationSteps.java
@@ -0,0 +1,8 @@
+public class CompilationSteps {
+    public static void main(String[] args) {
+        System.out.println("Step 1 : Write a java source Code");
+        System.out.println("Step 2 : Compile code using javac into bytecode ");
+        System.out.println("Step 3 : compiler generates java bytecode");
+        System.out.println("Step 4 : The JVM executes the bytecode");
+    }
+}
