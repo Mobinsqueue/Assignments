4e2ea70049cacc06151ab77f31a61fbb023e486c
Mobinsqueue <150966273+Mobinsqueue@users.noreply.github.com>
2026-08-26 20:12:38 -0700

    Update part12.java

--- CISC 190 JAVA/Week 1/assignment 4/part12.java
+++ CISC 190 JAVA/Week 1/assignment 4/part12.java
@@ -1,5 +1,5 @@
 // 1. because int only gives whole number , use double for fraction
-// 2. making only one side into double
+// 2. making only one side of calculation into double
 
 
 //3. corrected code
